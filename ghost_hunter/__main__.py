a74b92e70ae668b6bfc19c6f4977ef64ab49d7ee
tylerdonison <tylerdonison@gmail.com>
2021-10-13 12:33:31 -0600

    added map, camera, player movement, and collision.

--- ghost_hunter/__main__.py
+++ ghost_hunter/__main__.py
@@ -1,1 +1,11 @@
-pass
+import arcade
+from game.setup import setup
+
+def main():
+    """Main function"""
+    window = setup()
+    window.setup()
+    arcade.run()
+
+if __name__ == "__main__":
+    main()
---
+++ ghost_hunter/game/setup.py
@@ -0,0 +1,139 @@
+import arcade
+from arcade import camera
+from game.constants import SCREEN_HEIGHT, SCREEN_TITLE, SCREEN_WIDTH 
+from game.constants import CHARACTER_SCALING, TILE_SCALING
+from game.constants import PLAYER_MOVEMENT_SPEED, PLAYER_START_X, PLAYER_START_Y
+from game import images
+
+class setup(arcade.Window):
+    """
+    Main application class.
+    """
+
+    def __init__(self):
+
+        # Call the parent class and set up the window
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+        
+        # TileMap Object
+        self.tile_map = None
+
+        # Our Scene Object
+        self.scene = None
+        
+        # Separate variable that holds the player sprite
+        self.player_sprite = None
+
+        # Our physics engine
+        self.physics_engine = None
+
+        # A Camera that can be used for scrolling the screen
+        self.camera = None
+
+        # A Camera that can be used to draw GUI elements
+        self.gui_camera = None
+
+        arcade.set_background_color(arcade.csscolor.BLACK)
+
+    def setup(self):
+        """Set up the game here. Call this function to restart the game."""
+        self.setup_camera()
+        self.draw_map()   
+        self.player_setup()
+        # Create the 'physics engine'
+        self.physics_engine = arcade.PhysicsEngineSimple(
+            self.player_sprite, self.scene.get_sprite_list("Tile Layer 1"))
+
+    def setup_camera(self):
+        """Setup the Cameras"""
+        
+        self.camera = arcade.Camera(self.width, self.height)
+        self.gui_camera = arcade.Camera(self.width, self.height)
+
+    def draw_map(self):
+        
+        #map_name = ":resources:tiled_maps/Apartment_2.3.json"
+        map_name = "game/images/Apartment_2.3.json"
+        # Layer specific options are defined based on Layer names in a dictionary
+        # Doing this will make the SpriteList for the platforms layer
+        # use spatial hashing for detection.        
+        layer_options = {
+            "Tile Layer 1": {
+                "use_spatial_hash": True,
+            },
+            # "Room Names":{
+            #     "use_spatial_hash": True,
+            # },
+        }        
+        # Read in the tiled map
+        self.tile_map = arcade.load_tilemap(map_name, TILE_SCALING, layer_options)
+        self.scene = arcade.Scene.from_tilemap(self.tile_map)
+        # Set the background color
+        if self.tile_map.tiled_map.background_color:
+            arcade.set_background_color(self.tile_map.tiled_map.background_color)
+    
+    def player_setup(self):
+        player_list = arcade.SpriteList()
+        # Set up the player, specifically placing it at these coordinates.
+        image_source = ":resources:images/animated_characters/female_adventurer/femaleAdventurer_idle.png"
+        self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCALING)
+        self.player_sprite.center_x = PLAYER_START_X
+        self.player_sprite.center_y = PLAYER_START_Y
+        self.scene.add_sprite("Player", self.player_sprite)
+
+    def on_draw(self):
+        """Render the screen."""
+
+        # Clear the screen to the background color
+        arcade.start_render()
+
+        # Activate the game camera
+        self.camera.use()
+
+        # Draw our Scene
+        self.scene.draw()
+
+        # Activate the GUI camera before drawing GUI elements
+        self.gui_camera.use()
+
+    def on_key_press(self, key, modifiers):
+        """Called whenever a key is pressed."""
+
+        if key == arcade.key.UP or key == arcade.key.W:
+            self.player_sprite.change_y = PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+            self.player_sprite.change_y = -PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
+
+    def on_key_release(self, key, modifiers):
+        """Called when the user releases a key."""
+
+        if key == arcade.key.UP or key == arcade.key.W:
+            self.player_sprite.change_y = 0
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+            self.player_sprite.change_y = 0
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = 0
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = 0
+
+    def center_camera_to_player(self):
+        screen_center_x = self.player_sprite.center_x - (self.camera.viewport_width / 2)
+        screen_center_y = self.player_sprite.center_y - (
+            self.camera.viewport_height / 2
+        )
+        if screen_center_x < 0:
+            screen_center_x = 0
+        if screen_center_y < 0:
+            screen_center_y = 0
+        player_centered = screen_center_x, screen_center_y
+
+        self.camera.move_to(player_centered)
+    
+    def on_update(self, delta_time):
+        self.physics_engine.update()
+
+        self.center_camera_to_player()
