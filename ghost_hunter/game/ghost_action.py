416b04ce2f98714fa1d9a7329d3c5e11c710eaf4
tylerdonison <86095868+tylerdonison@users.noreply.github.com>
2021-10-12 21:49:34 -0600

    Delete ghost_action.py

--- ghost_hunter/game/ghost_action.py
+++
@@ -1,9 +0,0 @@
-
-Class ghost_action:
-    def execute(self, cast):
-        """Executes the action using the given actors.
-
-        Args:
-            cast (dict): The game actors {key: tag, value: list}.
-        """
-        raise NotImplementedError("execute not implemented in superclass")
