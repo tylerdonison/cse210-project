189e966aeada4ab9e545a6ae4fb8f1b735d4a61d
mswain430 <millie.swain@gmail.com>
2021-10-14 15:32:51 -0700

    Update director.py

--- ghost_hunter/game/director.py
+++ ghost_hunter/game/director.py
@@ -1,1 +1,69 @@
-pass
+import math
+import os
+import arcade
+
+
+class Director:
+
+    def setup(self): 
+         ""Set up the game here. Call this function to restart the game."""
+         # -- Enemies
+        enemies_layer = self.tile_map.object_lists[LAYER_NAME_ENEMIES]
+
+       for my_object in enemies_layer:
+           cartesian = self.tile_map.get_cartesian(
+                my_object.shape[0], my_object.shape[1]
+            )
+            enemy_type = my_object.properties["type"]
+            if enemy_type == "robot":
+                enemy = RobotEnemy()
+            elif enemy_type == "zombie":
+                enemy = ZombieEnemy()
+            elif enemy_type == "ghost":
+                enemy = GhostEnemy()
+            enemy.center_x = math.floor(
+                cartesian[0] * TILE_SCALING * self.tile_map.tile_width
+            )
+            enemy.center_y = math.floor(
+                (cartesian[1] + 1) * (self.tile_map.tile_height * TILE_SCALING)
+            )
+            if "boundary_left" in my_object.properties:
+                enemy.boundary_left = my_object.properties["boundary_left"]
+            if "boundary_right" in my_object.properties:
+                enemy.boundary_right = my_object.properties["boundary_right"]
+            if "change_x" in my_object.properties:
+                enemy.change_x = my_object.properties["change_x"]
+            self.scene.add_sprite(LAYER_NAME_ENEMIES, enemy)
+    
+    def on_update(self, delta_time):
+        """Movement and game logic""" 
+            # Update enemies
+        self.scene.update([LAYER_NAME_ENEMIES])
+
+        # See if the enemy hit a boundary and needs to reverse direction.
+        for enemy in self.scene.get_sprite_list(LAYER_NAME_ENEMIES):
+            if (
+                enemy.boundary_right
+                and enemy.right > enemy.boundary_right
+                and enemy.change_x > 0
+            ):
+                enemy.change_x *= -1
+
+            if (
+                enemy.boundary_left
+                and enemy.left < enemy.boundary_left
+                and enemy.change_x < 0
+            ):
+                enemy.change_x *= -1
+                
+        # Update Animations
+        self.scene.update_animation(
+            delta_time,
+            [
+                #LAYER_NAME_COINS,
+                LAYER_NAME_BACKGROUND,
+                LAYER_NAME_PLAYER,
+                LAYER_NAME_ENEMIES,
+            ],
+        )
+         
