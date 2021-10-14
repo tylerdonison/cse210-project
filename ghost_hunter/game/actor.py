371d1866335e07ab3c1614f2251266f8023fc8bb
mswain430 <millie.swain@gmail.com>
2021-10-14 13:23:41 -0700

    Update actor.py

--- ghost_hunter/game/actor.py
+++ ghost_hunter/game/actor.py
@@ -1,3 +1,17 @@
+import math
+import os
+
+import arcade
+import pytiled_parser
+
+def load_texture_pair(filename):
+    """
+    Load a texture pair, with the second being a mirror image.
+    """
+    return [
+        arcade.load_texture(filename),
+        arcade.load_texture(filename, flipped_horizontally=True),
+    ]
 
 class Entity:
   passclass Entity(arcade.Sprite):
