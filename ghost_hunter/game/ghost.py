0a00c14be590cc9f85f3cc4725637663fd66f14f
mswain430 <millie.swain@gmail.com>
2021-10-20 16:56:20 -0700

    Update ghost.py

--- ghost_hunter/game/ghost.py
+++ ghost_hunter/game/ghost.py
@@ -109,3 +109,38 @@ class Ghost(Entity):
 
     def move_ghost(self):
         pass
+    
+    def follow_sprite(self, player_sprite):
+        """
+        This function will move the current sprite towards whatever
+        other sprite is specified as a parameter.
+
+        We use the 'min' function here to get the sprite to line up with
+        the target sprite, and not jump around if the sprite is not off
+        an exact multiple of SPRITE_SPEED.
+        """
+
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+
+        # Random 1 in 100 chance that we'll change from our old direction and
+        # then re-aim toward the player
+        if random.randrange(100) == 0:
+            start_x = self.center_x
+            start_y = self.center_y
+
+            # Get the destination location for the Ghost
+            dest_x = player_sprite.center_x
+            dest_y = player_sprite.center_y
+
+            # Do math to calculate how to get the Ghost to the destination.
+            # Calculation the angle in radians between the start points
+            # and end points. This is the angle the Ghost will travel.
+            x_diff = dest_x - start_x
+            y_diff = dest_y - start_y
+            angle = math.atan2(y_diff, x_diff)
+
+            # Taking into account the angle, calculate our change_x
+            # and change_y. Velocity is how fast the Ghost travels.
+            self.change_x = math.cos(angle) * COIN_SPEED
+            self.change_y = math.sin(angle) * COIN_SPEED
